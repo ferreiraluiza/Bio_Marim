0c035dbd028800d3aa9865c3e6cc5c04dce15687
ferreiraluiza <annaluizaferreira1306@gmail.com>
2025-05-07 21:46:20 -0300

    mexendo

---
+++ bio_marim/src/App.jsx
@@ -0,0 +1,54 @@
+import "./app.scss" 
+
+function App() {
+  return (
+    <div className="div-mae">
+      <div className="img-1">
+        <img src="" alt="" />
+      </div>
+
+      <section className="sc-1">
+        <div className="images">
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+
+        <p className="txt-1">
+          Nossa equipe conta com pesquisadores, voluntarios, veterinarios e doadores, e você pode fazer parte disso!
+        </p>
+
+        <button>Queero Doar</button>
+        <button>Quero Ser Voluntário</button>
+      </section>
+
+      <section className="sc-sobre">
+        <img src="" alt="" />
+        <p className="txt-2">Somos uma organização dedicada à conservação dos ecossistemas marinhos, promovendo pesquisas, educação ambiental e ações de preservação. Nosso objetivo é proteger a biodiversidade dos oceanos, conscientizar a sociedade sobre a importância da vida marinha e incentivar práticas sustentáveis. Com o apoio de voluntários, cientistas e parceiros, trabalhamos para garantir um futuro mais azul para as próximas gerações.</p>
+
+        <button>Ver Mais</button>
+      </section>
+
+      <section className="sc-parc">
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </section>
+
+      <section className="sc-vcsabia">
+        <div className="qd-1"> Mais de 80% do oceano ainda não foi explorado Grande parte do fundo do mar permanece um mistério, o que significa que há muitas espécies e ecossistemas que ainda não conhecemos.</div>
+
+        <div className="qd-2">Os peixes-palhaço nascem todos machos. Esses peixes podem mudar de sexo conforme necessário! Se a fêmea dominante morre, o macho mais forte se transforma em fêmea para assumir seu lugar no grupo.</div>
+
+        <div className="qd-3"> As baleias azuis são os maiores animais da Terra Elas podem chegar a 30 metros de comprimento e pesar até 200 toneladas. Seu coração pode ser do tamanho de um carro!</div>
+
+        <div className="divulg">
+          <h1>QUER SABER MAIS SOBRE A BIOLOGIA MARINHA?</h1>
+          <h2>Siga nossos canais de comunicação e redes sociais e mergulhe em curiosidades sobre o oceano!</h2>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ bio_marim/src/pages/sobre_nos/index.jsx
@@ -0,0 +1,49 @@
+import "./index.scss"
+
+function Volunt() {
+    return (
+        <div className="div-pai">
+            <section className="sc-sobre">
+                <h1>SOBRE NÓS</h1>
+
+                <p className="txt-1">
+                    A BioMarim é uma organização sem fins lucrativos dedicada à conservação e à pesquisa da vida marinha. Nosso trabalho é movido pela paixão pelos oceanos e pelo compromisso em garantir um futuro sustentável para as próximas gerações.
+                </p>
+
+            </section>
+
+            <section className="sc-mvv">
+                <div className="missao">
+                    <img src="" alt="" />
+
+                    <h2>MISSÃO</h2>
+                    <p className="txt-2">Proteger e preservar os ecossistemas marinhos por meio da educação ambiental, da pesquisa científica e do engajamento das comunidades costeiras.</p>
+                </div>
+
+                <div className="visao">
+                    <img src="" alt="" />
+
+                    <h2>VISÃO</h2>
+                    <p className="txt-3">Ser referência nacional na promoção da consciência ambiental e na proteção da biodiversidade marinha, contribuindo para oceanos mais saudáveis e resilientes.</p>
+                </div>
+
+                <div className="valores">
+                    <img src="" alt="" />
+
+                    <h2>VALORES</h2>
+                    <p className="txt-4">Sustentabilidade: atuamos com responsabilidade ecológica, promovendo práticas que respeitam os limites do planeta.
+
+                        Educação: acreditamos que o conhecimento transforma e é essencial para a conservação ambiental.
+
+                        Transparência: mantemos uma gestão ética e acessível, com foco na confiança da sociedade.
+
+                        Colaboração: unimos forças com comunidades, cientistas e voluntários para ampliar nosso impacto.
+
+                        Respeito à vida: valorizamos todas as formas de vida marinha e lutamos por sua proteção.</p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Volunt();
